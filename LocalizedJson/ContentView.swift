9e2fa47d87fee5ae610f6bc1352e901dde9a87ef
Gustavo Carreno <gustavo.carreno@kadevjo.com>
2021-01-25 20:37:02 -0600

    Initial Commit

---
+++ LocalizedJson/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LocalizedJson
+//
+//  Created by Gustavo Carreno on 25/1/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LocalizedJson/LocalizedJsonApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalizedJsonApp.swift
+//  LocalizedJson
+//
+//  Created by Gustavo Carreno on 25/1/21.
+//
+
+import SwiftUI
+
+@main
+struct LocalizedJsonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
